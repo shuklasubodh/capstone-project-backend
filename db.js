c3c279ff538ac9813ac1a97ac225ec642e523ed7
Subodh Shukla <SubodhShukla@gmail.com>
2026-07-23 20:16:35 +0800

    updated backend.js

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
-const { Pool } = require("pg");
+import pg from "pg";
+const { Pool } = pg;
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
@@ -8,4 +9,4 @@ const pool = new Pool({
             : false,
 });
 
-module.exports = pool;
+export default pool;
